d140156960f04dcc5fe5c1b72686e579d503433d
Septentrion50 <erwan56lecorre@gmail.com>
2021-11-02 15:39:18 +0100

    Feature: pub_key_sys

---
+++ pub_key_sys.js
@@ -0,0 +1,18 @@
+const { generateKeyPairSync } = require('crypto');
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+  modulusLength: 2048, // The length of the key in bits
+  publicKeyEncoding: {
+    type: 'spki', // Recommended by Node.js docs
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8', // Recommended by Node.js docs
+    format: 'pem',
+    // cipher: 'aes-256-cbc,
+    // passphrase: 'top-secret,
+  }
+});
+
+console.log(publicKey);
+console.log(privateKey);
